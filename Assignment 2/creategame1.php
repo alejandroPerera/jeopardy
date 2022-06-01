509c893dbe29fb9d699b13b2f101825ffcabb983
chen-wen-tao <66928428+chen-wen-tao@users.noreply.github.com>
2022-05-31 22:41:11 -0400

    fix

--- Assignment 2/creategame1.php
+++ Assignment 2/creategame1.php
@@ -17,6 +17,13 @@
     ?>
     <form>
         <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6 big_label">
+                <label class="big_label">Create your own game</label>
+            </div>
+            <div class="col-md-3"></div>
+        </div>
+        <div class="row">
             <div class="col-md-4"></div>
             <div class="col-md-4">
                 <div class="create_game">
