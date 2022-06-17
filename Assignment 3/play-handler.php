ea64ed677db722eac4aed41ee8cb62f559ee86ad
chen-wen-tao <66928428+chen-wen-tao@users.noreply.github.com>
2022-06-17 03:37:02 -0400

    fix

--- Assignment 3/play-handler.php
+++ Assignment 3/play-handler.php
@@ -10,6 +10,7 @@
 
         $result = $db->query(" SELECT * FROM jeopardy_game_questions WHERE title= '$title' AND author= '$author' ORDER BY `jeopardy_game_questions`.`category` ASC , `jeopardy_game_questions`.`points` ASC;");
         $results = $result->fetchAll();
+        echo sizeof($results);
         if (sizeof($results) < 25){
             // echo $results[0]['question'];
             echo("<script type='text/javascript'>alert ('this game is unfinished yet');</script>");
@@ -17,14 +18,18 @@
             header('refresh:0; url=index.php');
         }
         else{
+            
             for ($i = 0; $i < 25; $i++ ){
-                setcookie('question'.$i+$tmp+1, $results[$i]['question'], time()+3600);
+                $tmp = 'question'.($i+$tmp+1);
+                setcookie($tmp, $results[$i]['question'], time()+3600);
             }
             for ($i = 0; $i < 5; $i++){
-                setcookie('points'.$i+1, $results[$i]['points'], time()+3600);
+                $point_tmp = 'points'.($i+1);
+                setcookie($point_tmp, $results[$i]['points'], time()+3600);
             }
             for ($i = 0; $i < 5; $i++){
-                setcookie('category'.$i+1, $results[$i*5]['category'], time()+3600);
+                $category_tmp = 'category'.($i+1);
+                setcookie($category_tmp, $results[$i*5]['category'], time()+3600);
             }
             header("refresh:0; url=playgame.php");
         }
